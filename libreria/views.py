7c56f0a4c4158e733a16007c63557fedb790eba6
José Alberto Gómez García <modej@correo.ugr.es>
2023-03-16 10:59:59 +0100

    [#6] Add searcher method

--- libreria/views.py
+++ libreria/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from libreria.models import Libro
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'base.html', context=None)
 
-def buscar(request):
-    # Se supone que aquí se haría la búsqueda
-    return render(request, 'buscar.html', context=None)
+def busqueda(request, query):
+    libros = Libro.objects.filter(title__icontains=query)
+    # Aquí vendría la plantilla que muestra los resultados
+    context = {
+        'busqueda': query,
+        'libros': libros
+        }
+    return render(request, 'busqueda.html', context=context)
