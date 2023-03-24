6043f0c52305c8fcbbcce733550c0ef21fdeca54
José Alberto Gómez García <modej@correo.ugr.es>
2023-03-24 19:35:58 +0100

    [#9] Add and details :construction:

--- libreria/urls.py
+++ libreria/urls.py
@@ -3,6 +3,7 @@ from libreria import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('buscar/<str:query>', views.busqueda, name='busqueda'),
-    path('buscar/', views.buscar, name='buscar')
+    path('buscar/', views.buscar, name='buscar'),
+    path('añadir/', views.añadir, name='añadir'),
+    path('detalle/<int:id>', views.detalle, name='detalle'),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -1,22 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from libreria.models import Libro
+from libreria.forms import FormularioLibro
+import logging
+logger = logging.getLogger(__name__)
 
-# Imagino que en algún momento se sustituirá por un index hecho y derecho.
+# Por ahora he puesto de página de inicio el listado de libros.
 def home(request):
-    return render(request, 'base.html', context=None)
+    return buscar(request)
 
-def busqueda(request, query):
+def buscar(request):
+    query = request.POST.get('query')
+    if query is None:
+        query = ""
     libros = Libro.objects.filter(title__icontains=query)
+    logger.info("Buscando: %s", query)
+    # Debería hacerse una validación de la query, y si es vacía mostrar otro menú de buscador.
+    # Pero por ahora no lo ha dicho, asi que hago el apaño para que en query vacia muestre todo.
     context = {
         'busqueda': query,
         'libros': libros
         }
     return render(request, 'busqueda.html', context=context)
 
-def buscar(request):
-    query = request.POST.get('query')
-    if query is None:
-        query = ""
-    # Debería hacerse una validación de la query, y si es vacía mostrar otro menú de buscador.
-    #  Pero por ahora no lo ha dicho, asi que hago el apaño para que en query vacia muestre todo.
-    return busqueda(request, query)
+def añadir(request):
+    formulario = FormularioLibro()
+    if request.method == 'POST':
+        formulario = FormularioLibro(request.POST)
+        if formulario.is_valid():
+            logger.debug("Añadiendo libro: %s", formulario.cleaned_data)
+            formulario.save()      # Esto hay que cambiarlo, el formulario no tiene método save
+            return redirect('buscar')
+    context = {
+        'form': formulario
+    }
+    return render(request, 'añadir.html', context=context)
+
+def detalle(request, id):
+    # Buscar en la BD
+    context = {
+        'id': id,
+    }
+    return render(request, 'detalle.html', context=context)
