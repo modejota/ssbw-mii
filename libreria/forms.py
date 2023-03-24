97ac6e9d8c9c557e66cb7baa810517c90bb0a587
José Alberto Gómez García <modej@correo.ugr.es>
2023-03-24 19:33:17 +0100

    [#9] Book form :construction:

---
+++ libreria/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class FormularioLibro(forms.Form):
+    title = forms.CharField(max_length=120, required=True, label="Título")
+    author = forms.CharField(max_length=120, required=True, label="Autor")
+    genre = forms.CharField(max_length=120, required=True, label="Género")
+    description = forms.CharField(max_length=240, required=True, label="Descripción")
+    isbn = forms.CharField(max_length=13, required=True, label="ISBN")
+    image = forms.CharField(max_length=240, required=True, label="Imagen")
+    published = forms.DateTimeField(required=False, label="Fecha de publicación",
+                                    widget=forms.DateTimeInput(
+                                        attrs={'class': 'form-control datetimepicker-input'},
+                                        format='%Y-%m-%d'))
+    publisher = forms.CharField(max_length=120, required=True, label="Editorial")
