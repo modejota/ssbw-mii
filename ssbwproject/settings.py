81cc882528fdf0e36d46d67e935f2a5dbc61306c
José Alberto Gómez García <modej@correo.ugr.es>
2023-03-24 19:22:10 +0100

    [#10] Append to log file

--- ssbwproject/settings.py
+++ ssbwproject/settings.py
@@ -160,7 +160,7 @@ LOGGING = {
 			'class': 'logging.FileHandler',
 			'filename': BASE_DIR / LOG_FILE,
 			'formatter': 'verbose',
-			'mode': 'w'   # sobreescribe cada vez
+			'mode': 'a'   # append to file
 			},
 
 		'console': {
