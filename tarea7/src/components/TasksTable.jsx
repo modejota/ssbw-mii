a64eea9d9f7f27dd072780b803897f5de8ac4cef
José Alberto Gómez García <modej@correo.ugr.es>
2023-05-16 19:45:54 +0200

    [#25] Finish TaskTable with Add and change status

--- tarea7/src/components/TasksTable.jsx
+++ tarea7/src/components/TasksTable.jsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -6,6 +6,7 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
+// STYLES
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
@@ -19,33 +20,46 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
+  }
 }));
 
+const AddTaskSectionStyles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: '1vh',
+    marginBottom: '1vh',
+    marginLeft: '1.5vw',
+  },
+  button: {
+    marginLeft: '10px',
+  },
+};
+
+
+// COMPONENTS
 const TableHeader = () => {
   return (
     <TableHead>
-        <TableRow>
-            <StyledTableCell align='center'>Task</StyledTableCell>
-            <StyledTableCell align='center'>Status</StyledTableCell>
-            <StyledTableCell align='center'></StyledTableCell>
-        </TableRow>
+      <TableRow>
+        <StyledTableCell align='center'>Task</StyledTableCell>
+        <StyledTableCell align='center'>Status</StyledTableCell>
+        <StyledTableCell align='center'></StyledTableCell>
+      </TableRow>
     </TableHead>
   );
 };
 
-const MyTableBody = ({tasks, handleDelete}) => {
+const MyTableBody = ({ tasks, handleDelete, changeTaskStatus }) => {
   const rows = tasks.map((row, index) => {
     return (
       <StyledTableRow key={index}>
         <StyledTableCell component="th" scope="row" align='center'>{row.title}</StyledTableCell>
         <StyledTableCell align="center">{row.status}</StyledTableCell>
         <StyledTableCell align="center">
-          <Button variant='contained' color='primary' size='small' onClick={() => handleDelete(index)}>Delete</Button>
+          <Button variant='contained' color='primary' size='small' onClick={() => changeTaskStatus(index)}>Change Status</Button>
+          <span style={{ marginRight: '1.5vw' }}></span>
+          <Button variant='contained' color='error' size='small' onClick={() => handleDelete(index)}>Delete</Button>
         </StyledTableCell>
       </StyledTableRow>
     );
@@ -53,13 +67,26 @@ const MyTableBody = ({tasks, handleDelete}) => {
   return <TableBody>{rows}</TableBody>
 };
 
-const TasksTable = ({tasks, handleDelete}) => {
-    return (
+const AddTaskSection = ({ textFieldValue, handleTextFieldChange, handleAdd }) => {
+  return (
+    <div style={AddTaskSectionStyles.container}>
+      <TextField id="outlined-basic" label="Task to be done" variant="outlined"
+        value={textFieldValue} onChange={handleTextFieldChange} />
+      <Button variant='contained' color='success' style={AddTaskSectionStyles.button} onClick={handleAdd}>Add Task</Button>
+    </div>
+  );
+};
+
+const TasksTable = ({ tasks, handleDelete, changeTaskStatus, handleAdd, textFieldValue, handleTextFieldChange }) => {
+  return (
+    <div>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHeader />
-        <MyTableBody tasks={tasks} handleDelete={handleDelete}/>
+        <MyTableBody tasks={tasks} handleDelete={handleDelete} changeTaskStatus={changeTaskStatus} />
       </Table>
-    );
+      <AddTaskSection textFieldValue={textFieldValue} handleTextFieldChange={handleTextFieldChange} handleAdd={handleAdd} />
+    </div>
+  );
 }
 
 export default TasksTable;
