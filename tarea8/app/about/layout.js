a32dea3ccf66d005b1951761c4ed7478276ed1ba
José Alberto Gómez García <modej@correo.ugr.es>
2023-05-23 23:39:07 +0200

    [#26] Modify sub-nav link

--- tarea8/app/about/layout.js
+++ tarea8/app/about/layout.js
@@ -3,7 +3,9 @@ import styles from '../page.module.css'
 export default function AboveLayout({ children }) {
     return (
         <>
-            <nav>Nav About</nav>
+            <nav>
+                <p style={{ marginLeft: '3vw' }}>Nav About</p>
+            </nav>
             <main className={styles.main}>
                 {children}
             </main>
